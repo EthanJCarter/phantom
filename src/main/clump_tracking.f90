3f15a027573c1c0213875186ec648a2b6fd2513c
Ethan Carter <ethan.j.carter@outlook.com>
2024-03-13 14:53:25 +0000

    removed extra whitespace found in some outfile names and fixed logfile output which caused an I/O error

--- src/main/clump_tracking.f90
+++ src/main/clump_tracking.f90
@@ -246,8 +246,8 @@ module clump_tracking
             averho  = (sum(clumpi%rho))/40 !Size of clumpi is hardcoded for ease for now.
             avetemp = (sum(clumpi%temp))/40
             print '(A, I, E10.3)', 'Average density for clump', w, (averho*unit_density)
-            print '(A,I3.3,A10)', 'Writing to: ', w, '.ave.dat'
-            write(clump_info_file, '(I3.3,A10)') w, ".ave.dat"
+            print '(A,I3.3,A8)', 'Writing to: ', w, '.ave.dat'
+            write(clump_info_file, '(I3.3,A8)') w, ".ave.dat"
             open(499,file=clump_info_file,position='append')
             write(499, '(I10, 5E10.3, 6F12.3)') clump_pid(w), time,  &
              (clump_dens(w)*unit_density), clump_temp(w), (averho*unit_density), avetemp,  &
@@ -269,12 +269,12 @@ module clump_tracking
              write(dumpfile_extension, ' (I2)')int(abs(log10(full_output_density(w))) * 10)
              write(clump_id, ' (I5)')w
              write(particle_id,' (I7)')clump_pid(w)
-             format = "(A4,A1,I3.3,A1,I3.3,A1,I7.7)"
+             format = "(A3,A1,I3.3,A1,I3.3,A1,I7.7)"
              write(dumpfile,format)runid,".",w, ".",(int(abs(log10(full_output_density(w))) * 10)),".",clump_pid(w)
              call write_fulldump(time,dumpfile)
              !call write_restart_file()
              !Changed to a logfile-style format
-             write(clump_info,'(A10,I3.3,A1,I3.3,A4)') "info",".",(int(abs(log10(full_output_density(w))) * 10)),".log"
+             write(clump_info,'(A4,A1,I3.3,A4)') "info",".",(int(abs(log10(full_output_density(w))) * 10)),".log"
              open(7228,file=clump_info,position='append')
              write(7228,*) "Number of clumps:", n_clumps
              write(7228,*) "Number of sinks:", nptmass
